34792dad04077d3febad34594cf066f8d931ac38
AnneKatie <annecarterk@gmail.com>
2025-10-23 23:47:30 -0700

    added 5 successful group tests

--- backend/lab-work/src/models/group.py
+++ backend/lab-work/src/models/group.py
@@ -1,32 +1,29 @@
 import random
+import string
 
 class Group:
 
-    def __new__(cls, songs: list[str], cover: str, title: str):
-        print("-- New is called --")
-        instance = super().__new__(cls) #idk if this is needed as a function? 
-        return instance
-
-    def __init__(self, ownerID: str, memberIDs: list[str], plistBoard: list[str], ownerUID: str, desc: str):
+    def __init__(self, ownerID: str, memberIDs: list[str], plistBoard: list[str], desc: str, groupName: str):
         self._ownerID = ownerID
         self._memberIDs = memberIDs
         self._plistBoard = plistBoard
-        self._ownerUID = ownerUID
         self._description = desc
+        self._groupName = groupName
         self.maxPLists = 20
         self.maxMembers = 20
         self.groupID = ''.join (random.choices(string.digits, k=10)) #Make a random 10 digit groupID
 
-    @classmethod
+    def __eq__(self, other) -> bool:
+        return self._ownerID == other._ownerID, self._memberIDs ==  other._memberIDs, self._plistBoard == other._plistBoard, self._description == other._description, self._groupName == other._groupName
 
     def inviteMember(self, callerID: str, inviteeID: str):
         #Error Handling
         errorCaught: bool = False
         if callerID not in self._memberIDs:
-            raise Exception("User {callerID} is not a member of group {self.groupID}")
+            raise Exception("User {callerID} is not a member of group {self._groupName}")
             errorCaught = True
         if inviteeID in self._memberIDs: 
-            raise Exception("User {inviteeID} is already a member of group {self.groupID}")
+            raise Exception("User {inviteeID} is already a member of group {self._groupName}")
             errorCaught = True
         if len(self._memberIDs) == self.maxMembers:
             raise Exception("Group is already at maximum capacity, cannot add more members.")
