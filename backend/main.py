d50fc30d6c7fabed0c4b75dbcc6a35e1278308ef
Andrew Collins <56804602+aceeedev@users.noreply.github.com>
2025-10-29 10:54:12 -0700

    Setup basic flask

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/api/data')
+def get_data():
+    data = {
+        "name": "Alice",
+        "age": 30,
+        "city": "New York"
+    }
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
