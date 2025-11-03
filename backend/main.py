de418036755e2d712a8692806080fc467c40426a
AnneKatie <annecarterk@gmail.com>
2025-11-02 17:28:29 -0800

    Moved object functions from classes to main.py
    Moved classes from group, playlist, and user into main.py and added corresponding comments.

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,11 @@ from flask_cors import CORS
 import FireStoreInterface as FSI
 from spotifyInterface import SpotifyManager
 
+from models import group
+from models import playlist
+from models import song
+from models import user
+
 
 app = Flask(__name__)
 CORS(app)
@@ -54,3 +59,85 @@ def get_song(songID):
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+### Class Functions ###
+
+##Transfered From Group.py##
+def inviteMember(self: group, callerID: str, inviteeID: str):
+    #Error Handling
+    errorCaught: bool = False
+    if callerID not in self.memberIDs:
+        raise Exception("User {callerID} is not a member of group {self.groupName}")
+        errorCaught = True
+    if inviteeID in self.memberIDs: 
+        raise Exception("User {inviteeID} is already a member of group {self.groupName}")
+        errorCaught = True
+    if len(self.memberIDs) == self.maxMembers:
+        raise Exception("Group is already at maximum capacity, cannot add more members.")
+        errorCaught = True
+    if errorCaught: #Allows for multiple failure to be displayed. 
+        return
+    
+    self.memberIDs.append(inviteeID) #add user
+
+def add_to_shelf(self: group, playlist): #UC1
+    if playlist not in self.shelf:
+        self.shelf.append(playlist)
+
+
+def take_down_plist(self: group, playlist): #UC1
+    if playlist not in self.shelf:
+        raise Exception("Cannot remove playlist that is not on the shelf.")
+        errorCaught = True
+    else:
+        self.shelf.remove(playlist)
+
+##Transferred from Playlist.py##
+def add_to_library(self: playlist, user): #UC1
+    if self not in user.library:
+        user.save_playlist(self)
+    else:
+        raise Exception("User {user} already has a copy of {self.title}")
+        errorCaught = True
+
+
+def export_to_spotify(self: playlist, spotify_client):
+    """Export the playlist to Spotify using the provided Spotify client."""
+    pass
+
+def get_playlist(self: playlist):
+    return self
+
+def add_song(self: playlist, song: Song):
+    if song not in self.songs:
+        self.songs.append(song)
+
+def remove_song(self: playlist, song: Song):
+    self.songs = [obj for obj in self.songs if obj != song]
+
+
+def change_cover(self: playlist, new_cover: str):
+    self.cover = new_cover
+
+def change_title(self: playlist, new_title: str):
+    self.title = new_title
+
+## From user.py ##
+
+def spotify_login(self: user, spotify_user):
+    pass
+
+def respond_to_complaint(self: user, complaint_id: str, response: str, action: str) -> bool:
+    return self.isAdmin
+
+'''
+def create_complaint(self, complaint: AdminComplaint):
+    self.myComplaints.append(complaint)
+'''
+
+def remove_complaint(self: user, id: str):
+    # create a new array with all complaints except the one with the given id
+    self.myComplaints = [obj for obj in self.myComplaints if obj.id != id]
+
+def save_playlist(self: user, playlist):
+    self.library.append(playlist) #UC1. User has playlist relationship checked in playlist
