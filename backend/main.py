ba9c5dcf2ee1b92b95e01169495fc20382b6eabd
Ephraim esson <ephraimesson@icloud.com>
2025-11-05 15:17:40 -0800

    I added folders into fronend/src/pages all of these expalin what the pages are and what they need to do

--- backend/main.py
+++ backend/main.py
@@ -394,4 +394,4 @@ def take_playlist_from_group():
     
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
+import Home from './pages/landing/home.tsx'
+import Login from './pages/loginPage/login.tsx'
 import App from './App.tsx'
 import TestPage from './pages/TestPage.tsx'
 import TestAuthPage from './pages/TestAuthPage.tsx'
@@ -9,7 +11,9 @@ import Callback from './pages/CallbackPage.tsx';
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/app" element={<App />} />
       <Route path="/test" element={<TestPage />} />
       <Route path="/auth" element={<TestAuthPage />} />
       <Route path="/callback" element={<Callback />} />
