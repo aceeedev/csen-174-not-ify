fa72a405d8c1b43e9df9a09f28b82d1e9edf41e1
Andrew Collins <56804602+aceeedev@users.noreply.github.com>
2025-11-09 21:45:52 -0800

    Added check to redirect off onboarding if needed

--- frontend/src/pages/onboardingPage/OnboardingPage.tsx
+++ frontend/src/pages/onboardingPage/OnboardingPage.tsx
@@ -1,5 +1,41 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { auth, db } from '../../firebase';
+
 
 function OnboardingPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      // if the user already has the Spotify access token, return to the main page
+
+      if (currentUser) {
+        const userId = currentUser.uid;
+        const userDocRef = doc(db, "Users", userId);
+            
+        try {
+          const userDocSnap = await getDoc(userDocRef);
+          
+          if (userDocSnap.exists()) {
+            const userData = userDocSnap.data();
+            
+            if (userData.access_token) {
+              // the user has an access_token -> redirect to home page
+              navigate("/");
+            }
+          }
+        } catch (error) {
+          console.error("Error checking Firestore document:", error);
+        }
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   const signInToSpotify = async () => {
     const res = await fetch("http://localhost:5001/spotify/auth-url");
     const data = await res.json();
